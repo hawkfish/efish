ac41c523eb35fc8498ae3e0dd1e3679c6c28b26b
snapper <snapper>
2000-06-14 22:43:38 +0000

    Removed unnecessary ExecuteCommandNumber

--- PhotoPrinter/Source/Commands/PhotoDocCommandAttachment.cp
+++ PhotoPrinter/Source/Commands/PhotoDocCommandAttachment.cp
@@ -1,21 +1,33 @@
+/*
+	File:		PhotoDocCommandAttachment.cp
+
+	Contains:	Implementation of a command relating to the document.
+
+	Written by:	Dav Lion and David Dunham
+
+	Copyright:	Copyright ©2000 by Electric Fish, Inc.  All Rights reserved.
+
+	Change History (most recent first):
+
+		14 Jun 2000		drd		Removed unnecessary ExecuteCommandNumber
+*/
+
 #include "PhotoDocCommandAttachment.h"
 #include "PhotoPrintDoc.h"
 
-PhotoDocCommandAttachment::PhotoDocCommandAttachment(CommandT inCommand, PhotoPrintDoc* inDoc) 
+/*
+PhotoDocCommandAttachment
+*/
+PhotoDocCommandAttachment::PhotoDocCommandAttachment(const CommandT inCommand, PhotoPrintDoc* inDoc) 
 	: ECommandAttachment (inCommand)
 	, mDoc (inDoc)
 {
 	mDoc->AddAttachment(this);
 }//end ct
 
-
+/*
+~PhotoDocCommandAttachment
+*/
 PhotoDocCommandAttachment::~PhotoDocCommandAttachment() 
 {
 }//end dt
-
-
-void		
-PhotoDocCommandAttachment::ExecuteCommandNumber	(CommandT			/*inCommand*/,
-												 void*				/*inCommandData*/)
-{
-}
--- PhotoPrinter/Source/Commands/PhotoDocCommandAttachment.h
+++ PhotoPrinter/Source/Commands/PhotoDocCommandAttachment.h
@@ -1,24 +1,30 @@
-//-- PhotoDocCommandAttachment.h
-//-- Copyright © 2000 Electric Fish, Inc.
+/*
+	File:		PhotoDocCommandAttachment.h
+
+	Contains:	Definition of a command relating to the document.
+
+	Written by:	Dav Lion and David Dunham
+
+	Copyright:	Copyright ©2000 by Electric Fish, Inc.  All Rights reserved.
+
+	Change History (most recent first):
+
+		14 Jun 2000		drd		Removed unnecessary ExecuteCommandNumber
+*/
 
 #pragma once
+
 #include "ECommandAttachment.h"
 
 class PhotoPrintDoc;
 
 class PhotoDocCommandAttachment : public ECommandAttachment {
 	protected:
-		PhotoPrintDoc*	mDoc;
+		PhotoPrintDoc*	mDoc;					// Document we operate on
 		
 	public:
-							PhotoDocCommandAttachment(CommandT inCommand, 
+							PhotoDocCommandAttachment(const CommandT inCommand, 
 														PhotoPrintDoc* inDoc);
 														
 		virtual 			~PhotoDocCommandAttachment();
-		
-		virtual	void		ExecuteCommandNumber	(CommandT			inCommand,
-													 void*				inCommandData);
-													 
-													 
-		}; // end class PhotoDocCommandAttachment
-	
+}; // end class PhotoDocCommandAttachment
