a4bb4f7bc37c1b2bb8e3fc3930f95435fab74701
snapper <snapper>
2000-06-14 19:12:07 +0000

    Moved a couple files

--- PhotoPrinter/Source/PhotoPrintCommands.h
+++ PhotoPrinter/Source/PhotoPrintCommands.h
@@ -1,3 +1,19 @@
-// PhotoPrinterCommands.h
-// Copyright © 2000 Electric Fish, Inc.
+/*
+	File:		PhotoPrinterCommands.h
 
+	Contains:	Definitions of PowerPlant command IDs.
+
+	Written by:	David Dunham
+
+	Copyright:	Copyright ©2000 by Electric Fish, Inc.  All Rights reserved.
+
+	Change History (most recent first):
+
+		14 Jun 2000		drd		First commands
+*/
+
+enum {
+	// Options menu
+	cmd_BackgroundOptions	= 'bOpt',
+	cmd_ImageOptions		= 'iOpt'
+};
