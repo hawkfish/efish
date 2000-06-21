551d9301454a769b888a2f1436319e95b6bc92fe
slithy <slithy>
2000-06-21 15:24:09 +0000

    added 'okToExpand' to BestFit

--- Epp/EUtil.h
+++ Epp/EUtil.h
@@ -1,12 +1,26 @@
-//EUtil.h
+/*
+	File:		EUtil.
+
+	Contains:	utility routines
+
+	Written by:	Dav Lion and David Dunham
+
+	Copyright:	Copyright ©2000 by Electric Fish, Inc.  All Rights reserved.
+
+	Change History (most recent first):
+
+	
+	21 june 2000	dml		added 'okToExpand' to BestFit
+*/
 
 class EUtil {
 	public:
 	
 		static	void 	BestFit				(SInt32&	outWidth, 
 											 SInt32&	outHeight,
-											 const	SInt32		fitWidth,
-											 const	SInt32		fitHeight,
-											 const	SInt32		propWidth,
-											 const	SInt32		propHeight);
+											 const	SInt32		boundingWidth,
+											 const	SInt32		boundingHeight,
+											 const	SInt32		objectWidth,
+											 const	SInt32		objectHeight,
+													bool		okToExpand = true);
 	};//end class EUtil
