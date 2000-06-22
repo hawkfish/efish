e80fcf2e293c350e1009baea74595e0070792a87
snapper <snapper>
2000-06-22 17:55:42 +0000

    First checked in

---
+++ PhotoPrinter/Resources/Carbon.r
@@ -0,0 +1,24 @@
+/*
+	File:		Carbon.r
+
+	Contains:	Carbon-specific resources.
+
+	Written by:	Richard Wesley
+
+	Copyright:	Copyright ©2000 Electric Fish, Inc.  All Rights Reserved.
+
+	Change History (most recent first):
+
+		22 Jun 2000		drd		Created
+*/
+
+#include "Types.r"
+
+/*----------------------------carb € OSX Carbon Flag-------------------------------------*/
+type 'carb' {
+	longint = 0;
+};
+
+resource 'carb' (0)
+{
+};
