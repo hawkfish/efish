1c9bb7ef33433f285c1a4ae15cc35f3a39d1af29
snapper <snapper>
2000-06-27 21:54:46 +0000

    Added kLandscape, kPortrait

--- PhotoPrinter/Source/PhotoUtility.h
+++ PhotoPrinter/Source/PhotoUtility.h
@@ -1,15 +1,32 @@
-// PhotoUtility.h
-// Copyright © 2000 Electric Fish, Inc.
+/*
+	File:		PhotoUtility.h
 
+	Contains:	Handy functions and constants
+
+	Written by:	Dav Lion and David Dunham
+
+	Copyright:	Copyright ©2000 by Electric Fish, Inc.  All Rights reserved.
+
+	Change History (most recent first):
+
+		27 Jun 2000		drd		Added kLandscape, kPortrait
+*/
 
 #pragma once
+
 #include <math.h>
 
+enum {
+	kLandscape = 'land',
+	kPortrait = 'port'
+};
+
 class PhotoUtility {
-	public:
-		static const double sEpsilon;
-		static Boolean DoubleEqual(const double& a, const double& b) {return (fabs(a - b) < sEpsilon);};
-		static const RGBColor	sNonReproBlue;	
-	};//end class PhotoUtility
-	
-	
+public:
+	// Globals
+	static const double		sEpsilon;
+	static const RGBColor	sNonReproBlue;	
+
+	// Functions
+	static Boolean	DoubleEqual(const double& a, const double& b) { return (fabs(a - b) < sEpsilon); }
+};//end class PhotoUtility
