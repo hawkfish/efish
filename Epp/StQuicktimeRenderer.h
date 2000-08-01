d335cf361fed38ff1f2f9652cd1de98264e430f4
snapper <snapper>
2000-08-01 17:38:49 +0000

    Took out all sorts of extraneous includes

--- Epp/StQuicktimeRenderer.h
+++ Epp/StQuicktimeRenderer.h
@@ -9,31 +9,28 @@
 
 	Change History (most recent first):
 
+	01 Aug 2000		drd		Took out all sorts of extraneous includes
 	01 Aug 2000		drd		Removed unused mDest, added mDepth
 */
 
 #pragma once
 
-#include <UQuicktime.h>
-#include "MRect.h"
-#include "HORef.h"
-#include "StQTImportComponent.h"
 #include "UGWorld.h"
 
 class StQuicktimeRenderer : public StOffscreenGWorld {
-	protected:
-		SInt16			mDepth;
-		MatrixRecord*	mMat;
-		RgnHandle		mClip;
-		
-		void	Render();
-		
-	public:
-				 	StQuicktimeRenderer(const Rect&		inBounds,
-										SInt16			inPixelDepth = 0,
-										GWorldFlags		inFlags = useTempMem,
-										MatrixRecord*	pMat = 0,
-										RgnHandle		inClip = nil);
+protected:
+	SInt16			mDepth;
+	MatrixRecord*	mMat;
+	RgnHandle		mClip;
+	
+	void	Render();
+	
+public:
+			 	StQuicktimeRenderer(const Rect&		inBounds,
+									SInt16			inPixelDepth = 0,
+									GWorldFlags		inFlags = useTempMem,
+									MatrixRecord*	pMat = 0,
+									RgnHandle		inClip = nil);
 
-		virtual 	~StQuicktimeRenderer();
-	};//end StQuicktimeRenderer
+	virtual 	~StQuicktimeRenderer();
+};//end StQuicktimeRenderer
