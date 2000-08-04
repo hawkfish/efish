bd56eb053860e725aec85a67280ba9665f729723
snapper <snapper>
2000-08-04 21:20:58 +0000

    First checked in

---
+++ PhotoPrinter/Source/PhotoPrintConstants.h
@@ -0,0 +1,22 @@
+/*
+	File:		PhotoPrintConstants.h
+
+	Contains:	Miscellaneous constants.
+
+	Written by:	David Dunham
+
+	Copyright:	Copyright ©2000 by Electric Fish, Inc.  All Rights reserved.
+
+	Change History (most recent first):
+
+		04 Aug 2000		drd		Created
+*/
+
+#pragma once
+
+// PowerPlant
+enum {
+	// argument for LAction
+	kNotAlreadyDone = false,
+	kAlreadyDone = true
+};
---
+++ PhotoPrinter/Source/PhotoPrintResources.h
@@ -0,0 +1,39 @@
+/*
+	File:		PhotoPrintResources.h
+
+	Contains:	Resource constants.
+
+	Written by:	David Dunham
+
+	Copyright:	Copyright ©2000 by Electric Fish, Inc.  All Rights reserved.
+
+	Change History (most recent first):
+
+		04 Aug 2000		drd		Created
+*/
+
+#pragma once
+
+enum {
+	str_Redo = 259,							// 'STR#'
+	str_Undo = 260,							// 'STR#'
+		si_PasteImage = 1,
+		si_PastePicture,
+		si_PasteText,
+		si_DeleteImage,
+		si_CutImage,
+		si_DropImage,
+		si_DuplicateImage,
+		si_DropPicture,
+		si_DropText,
+		si_Nudge,
+		si_Move,
+		si_Copy,
+		si_Resize,
+		si_MoveToBack,
+		si_MoveToFront,
+		si_MoveBackward,
+		si_MoveForward,
+
+	rsrc_Fnord		// Allows us to put commas everywhere
+};
