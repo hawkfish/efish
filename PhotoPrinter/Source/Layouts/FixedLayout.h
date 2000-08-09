e5c43d33d1169715568845ee7d448e7b82fd76a5
snapper <snapper>
2000-08-09 23:15:07 +0000

    Moved mImageCount from SchoolLayout; changes for dialog

--- PhotoPrinter/Source/Layouts/FixedLayout.h
+++ PhotoPrinter/Source/Layouts/FixedLayout.h
@@ -10,6 +10,7 @@
 
 	Change History (most recent first):
 
+		09 Aug 2000		drd		Moved mImageCount from SchoolLayout; changes for dialog
 		26 Jun 2000		drd		GetNameIndex
 		23 Jun 2000		drd		Use HORef<PhotoPrintModel> in constructor
 		19 Jun 2000		drd		Created
@@ -20,9 +21,21 @@
 class FixedLayout : public Layout
 {
 public:
+	enum {
+		PPob_FixedOptions = 1103
+	};
+
 						FixedLayout(HORef<PhotoPrintModel>& inModel);
 	virtual 			~FixedLayout();
 
 	virtual	bool		CanAddToBackground(const UInt16 inCount);
+	virtual	void		CommitOptionsDialog(EDialog& inDialog);
+	virtual	ResIDT		GetDialogID() const					{ return PPob_FixedOptions; }
 	virtual	SInt16		GetNameIndex() const				{ return 3; }
+	virtual	void		SetupOptionsDialog(EDialog& inDialog);
+
+	virtual	void		SetImageCount(const UInt32 inCount);
+
+protected:
+	UInt32		mImageCount;
 };
