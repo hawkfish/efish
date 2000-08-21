a9fed99f3a27eeed611fc9bf2fece0a8e8c6c77b
snapper <snapper>
2000-08-21 22:42:29 +0000

    Added title-related data

--- PhotoPrinter/Source/DocumentProperties.h
+++ PhotoPrinter/Source/DocumentProperties.h
@@ -1,11 +1,21 @@
-//	DocumentProperties.h
-//		Copyright © 2000 Electric Fish, Inc. All rights reserved.
+/*
+	File:		DocumentProperties.h
 
-/******** Change Log (most recent first ***************
+	Contains:	Document-specific properties. (Why not just part of a document I'll never know.) 
+
+	Written by:	Dav Lion and David Dunham
+
+	Copyright:	Copyright ©2000 by Electric Fish, Inc.  All Rights reserved.
+
+	Change History (most recent first):
+
+		21 Aug 2000		drd		Added title-related data
 		11 Aug 2000		drd		Removed mFull, mEmpty
 */
 
 #pragma once
+
+#include "MPString.h"
 
 namespace XML {
 	class Output;
@@ -13,32 +23,56 @@ namespace XML {
 	class Handler;
 }
 
+enum TitlePositionT {
+		kHeader = 'head',
+		kFooter = 'foot',
+		kNoTitle = 'none'
+};
+
 class DocumentProperties {
-	public:
-		enum DisplayState {
-			kNormalState = 0,
-			kPreviewState,
-			kPrintingState,
-			kFnordState };
+public:
+	enum DisplayStateT {
+		kNormalState = 0,
+		kPreviewState,
+		kPrintingState,
+		kFnordState
+	};
 
-	protected:
-		bool			mDirty;
-		DisplayState 	mState;
-		
-	public:
-					DocumentProperties(bool inDirty, DisplayState inState);					
-					DocumentProperties(DocumentProperties& other);
-					DocumentProperties();
-		virtual		~DocumentProperties();
+protected:
+	bool			mDirty;
+	DisplayStateT 	mState;
+	TitlePositionT	mTitlePosition;
+	MPString		mTitle;
+	SInt16			mFontNumber;
+	SInt16			mFontSize;
+
+		static const char *const gTitleLabels[];
+	
+public:
+				DocumentProperties(bool inDirty, DisplayStateT inState);					
+				DocumentProperties(DocumentProperties& other);
+				DocumentProperties();
+	virtual		~DocumentProperties();
 
 
-					bool		 	GetDirty(void) const;
-					DisplayState 	GetState(void) const;
+				bool		 	GetDirty(void) const;
+				SInt16			GetFontNumber() const			{ return mFontNumber; }
+				SInt16			GetFontSize() const				{ return mFontSize; }
+				DisplayStateT 	GetState(void) const;
+				MPString&		GetTitle()						{ return mTitle; }
+				TitlePositionT	GetTitlePosition() const		{ return mTitlePosition; }
 
-					void		SetDirty(bool inVal);
-					void		SetState(DisplayState inVal);
+				void		SetDirty(const bool inVal);
+				void		SetFontName(ConstStr255Param inName);
+				void		SetFontNumber(const SInt16 inFont)	{ mFontNumber = inFont; }
+				void		SetFontSize(const SInt16 inSize)	{ mFontSize = inSize; }
+				void		SetState(const DisplayStateT inVal);
+				void		SetTitle(const MPString inTitle)	{ mTitle = inTitle; }
+				void		SetTitlePosition(const TitlePositionT inPos)	{ mTitlePosition = inPos; }
 
 // IO
-					void 		Write(XML::Output &out) const;
-					void 		Read(XML::Element &elem);
+		static	void		ParseProperties(XML::Element &elem, void *userData);
+		static 	void		ParseTitlePosition(XML::Element &elem, void *userData);
+				void 		Read(XML::Element &elem);
+				void 		Write(XML::Output &out) const;
 }; //end class DocumentProperties
