c35c4e016d7323f0545dae27c5c282bb94e7eaec
slithy <slithy>
2000-09-21 15:57:52 +0000

    use LStr255, ct takes ConstStr255Param

--- PhotoPrinter/Source/PhotoExceptionHandler.h
+++ PhotoPrinter/Source/PhotoExceptionHandler.h
@@ -9,25 +9,25 @@
 
 	Change History (most recent first):
 
+		21 sep 2000		dml		use LStr255, take ConstStr255Param as ct arg
 		20 sep 2000		dml		added SilentExceptionEater (subclass)
 		20 Sep 2000		dml		Created
 */
 
-#include "MPString.h"
 
 class PhotoExceptionHandler {
 	protected:
 		static PhotoExceptionHandler*	gCurrent;	
 		
 		PhotoExceptionHandler*	mPrevious;
-		MPString				mOperation;
+		LStr255					mOperation;
 
-		void	ReportException(const MPString& parm0, const MPString& parm1, 
-								const MPString& parm2, const MPString& parm3);
+		void	ReportException(const LStr255& parm0, const LStr255& parm1, 
+								const LStr255& parm2, const LStr255& parm3);
 		bool	HandleException(LException& e);
 		
 	public:
-				PhotoExceptionHandler(MPString inOperationName = "\pthe command");
+				PhotoExceptionHandler(LStr255 inOperationName = "\pthe command");
 		virtual ~PhotoExceptionHandler();
 	static bool	HandleKnownExceptions(LException& inException);
 	};//end
@@ -36,8 +36,8 @@ class PhotoExceptionHandler {
 	
 class SilentExceptionEater : public PhotoExceptionHandler {
 	protected:
-		void	ReportException(const MPString& /*parm0*/, const MPString& /*parm1*/, 
-								const MPString& /*parm2*/, const MPString& /*parm3*/) {};
+		void	ReportException(const LStr255& /*parm0*/, const LStr255& /*parm1*/, 
+								const LStr255& /*parm2*/, const LStr255& /*parm3*/) {};
 	public:
 				SilentExceptionEater() : PhotoExceptionHandler() {};
 		virtual ~SilentExceptionEater() {};
