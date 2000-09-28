c7a06630f917109e948803e7d0c86838cb609835
hawkfish <hawkfish>
2000-09-28 20:04:46 +0000

    Convert to swing hello world.

--- SwingCVS/SwingCVS.java
+++ SwingCVS/SwingCVS.java
@@ -2,17 +2,56 @@
 //  SwingCVS.java
 //
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class SwingCVS extends Frame {
-    public SwingCVS() {
-	super("SwingCVS");
-	Label l = new Label("Hello World!");;
-	add(l);
-	setVisible(true);
-    }
-    
-    public static void main(String args[]) {
-	new SwingCVS();
-    }
-}
+public class SwingCVS 
+	
+	{ // begin SwingCVS
+	
+	public Component
+	createComponents ()
+		
+		{ // begin createComponents
+		
+			final	JLabel	label = new JLabel ("Hello World!");
+			
+			JPanel pane = new JPanel ();
+			pane.setBorder (BorderFactory.createEmptyBorder (30, 30, 10, 30));
+			pane.setLayout (new GridLayout (0, 1));
+			pane.add (label);
+			
+			return pane;
+			
+		} // end createComponents
+		    
+    public static void 
+	main (String args[]) 
+	
+		{ // begin main
+			
+			try {
+				UIManager.setLookAndFeel (UIManager.getCrossPlatformLookAndFeelClassName ());
+				} // try
+				
+			catch (Exception e) {
+				} // catch
+				
+			JFrame	frame = new JFrame ("SwingCVS");
+			SwingCVS app = new SwingCVS ();
+			Component contents = app.createComponents ();
+			frame.getContentPane ().add (contents, BorderLayout.CENTER);
+			
+			frame.addWindowListener (new WindowAdapter () {
+				public void
+				windowClosing (WindowEvent e)
+					{System.exit (0);}
+				});
+				
+			frame.pack ();
+			frame.setVisible (true);
+			
+		} // end main
+
+	} // end SwingCVS
