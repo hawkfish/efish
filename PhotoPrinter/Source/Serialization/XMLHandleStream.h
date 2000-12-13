88fbcd84432b959563341b09b640a0eae82441dc
snapper <snapper>
2000-12-13 00:53:33 +0000

    13 Added constructors; now also an InputStream

--- PhotoPrinter/Source/Serialization/XMLHandleStream.h
+++ PhotoPrinter/Source/Serialization/XMLHandleStream.h
@@ -9,12 +9,22 @@
 
 	Change History (most recent first):
 	
+		12 Dec 2000		drd		13 Added constructors; now also an InputStream
 		30 Aug 2000		drd		Created
 */
 
 #include "xmloutput.h"
 
-class XMLHandleStream : public XML::OutputStream, public LHandleStream
+class XMLHandleStream : public XML::InputStream, public XML::OutputStream, public LHandleStream
 {
-	int write(const char *str, size_t len);
+public:
+		XMLHandleStream();
+		XMLHandleStream(Handle inHandle);
+
+protected:
+	// InputStream
+	virtual int read(XML_Char *buf, size_t bufLen);
+
+	// OutputStream
+	virtual	int write(const char *str, size_t len);
 };
