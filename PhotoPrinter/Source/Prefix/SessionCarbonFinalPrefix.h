3f74f43320ad9f5e9af79a87cc27dcca88bb86ee
slithy <slithy>
2001-02-26 21:47:50 +0000

    revamped targets

---
+++ PhotoPrinter/Source/Prefix/SessionCarbonFinalPrefix.h
@@ -0,0 +1,9 @@
+// ===========================================================================
+//	CarbonFinalPrefix.h		©2000 Electric Fish, Inc. All rights reserved.
+// ===========================================================================
+
+	// Bring in the project's precompiled header for the given target
+
+#define PP_DEBUG				0
+
+#include "SessionCarbonHeadersPPC++"
