64f7674947e231fc66bfef484a142ade9650bb0a
slithy <slithy>
2001-02-26 20:01:22 +0000

    AllowDontBeTarget replaces DontBeTarget

--- PhotoPrinter/Source/Controllers/FileEditText.h
+++ PhotoPrinter/Source/Controllers/FileEditText.h
@@ -8,6 +8,7 @@
 
 	Change History (most recent first):
 
+	26 feb 2001		dml			AllowDontBeTarget replaces DontBeTarget.
 	23 feb 2001		dml			created
 
 */
@@ -45,9 +46,9 @@ public:
 
 	virtual				~FileEditText();
 
-	virtual void		DontBeTarget();
+	virtual Boolean		AllowDontBeTarget(LCommander* inNewTarget);
 	virtual Boolean		HandleKeyPress(const EventRecord&	inKeyEvent);
 	virtual void		SetItem(PhotoItemRef inItem);
-
+	virtual bool		TryRename(void);
 	};//end FileEditText
 	
