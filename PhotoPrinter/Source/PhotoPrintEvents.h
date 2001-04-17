eb0006c790f2797d6a2438f5dc328930f9319a13
snapper <snapper>
2001-04-17 21:36:01 +0000

    Avoid duplicate ae_Preferences

--- PhotoPrinter/Source/PhotoPrintEvents.h
+++ PhotoPrinter/Source/PhotoPrintEvents.h
@@ -9,6 +9,7 @@
 
 	Change History (most recent first):
 
+		17 Apr 2001		drd		Avoid duplicate ae_Preferences
 		08 Nov 2000		drd		ae_Preferences
 		23 Aug 2000		drd		Created
 */
@@ -21,5 +22,10 @@ enum {
 
 	ae_Import = 4001,
 	
+	// ??? this is now defined in UAppleEventsMgr.h, and we should eventually transition
+#if ( __PowerPlant__ < 0x02114004 )
 	ae_Preferences = 4002		// For Aqua
+#else
+	ae_PPE_END					// Totally bogus
+#endif
 };
