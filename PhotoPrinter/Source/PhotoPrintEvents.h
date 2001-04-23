f82706e83c15f00a11378df150b24795155da73e
snapper <snapper>
2001-04-23 21:36:55 +0000

    Renumbered ae_Preferences to be same as PowerPlant 2.1.1

--- PhotoPrinter/Source/PhotoPrintEvents.h
+++ PhotoPrinter/Source/PhotoPrintEvents.h
@@ -9,6 +9,7 @@
 
 	Change History (most recent first):
 
+		23 Apr 2001		drd		Renumbered ae_Preferences to be same as PowerPlant 2.1.1
 		17 Apr 2001		drd		Avoid duplicate ae_Preferences
 		08 Nov 2000		drd		ae_Preferences
 		23 Aug 2000		drd		Created
@@ -24,7 +25,7 @@ enum {
 	
 	// ??? this is now defined in UAppleEventsMgr.h, and we should eventually transition
 #if ( __PowerPlant__ < 0x02114004 )
-	ae_Preferences = 4002		// For Aqua
+	ae_Preferences = 1006		// For Aqua (same as UAppleEventsMgr.h)
 #else
 	ae_PPE_END					// Totally bogus
 #endif
