87f448f89373f5eca42a146157139b63c1bd9c66
hawkfish <hawkfish>
2001-06-29 03:00:11 +0000

    Split up PhotoPrint.rsrc.

--- PhotoPrinter/Resources/Carbon.r
+++ PhotoPrinter/Resources/Carbon.r
@@ -3,22 +3,15 @@
 
 	Contains:	Carbon-specific resources.
 
-	Written by:	Richard Wesley
+	Written by:	David Dunham
 
-	Copyright:	Copyright ©2000 Electric Fish, Inc.  All Rights Reserved.
+	Copyright:	Copyright ©2000-1 Electric Fish, Inc.  All Rights Reserved.
 
 	Change History (most recent first):
 
+		29 Jun 2001		rmgw	Added text-based plst.
 		22 Jun 2000		drd		Created
 */
 
-#include "Types.r"
-
-/*----------------------------carb ¥ OSX Carbon Flag-------------------------------------*/
-type 'carb' {
-	longint = 0;
-};
-
-resource 'carb' (0)
-{
-};
+/*----------------------------plst ¥ OS X Property List-------------------------------------*/
+Read 'plst' (0, "OS X", purgeable) "PhotoPrint.plst";
---
+++ PhotoPrinter/Resources/Version.r
@@ -0,0 +1,38 @@
+/*
+	File:		Version.r
+
+	Contains:	Version resources.
+
+	Written by:	Richard Wesley
+
+	Copyright:	Copyright ©2001 Electric Fish, Inc.  All Rights Reserved.
+
+	Change History (most recent first):
+
+		29 Jun 2001		rmgw	Created.
+*/
+
+#include <Types.r>
+
+#define versMajor			1
+#define versMinor			0
+#define versBuild			67
+
+#define versKind			development
+#define versKindChar		"d"
+
+#define versMajorBCD		((versMajor/10)*0x10 + (versMajor%10))
+#define versShortString		$$Format("%d.%d" versKindChar "%d", versMajor, versMinor, versBuild)
+
+resource 'vers' (1) {
+	versMajorBCD, versMinor, versKind, versBuild, verUS,
+	versShortString,
+	versShortString " ©2001 Electric Fish, Inc."
+};
+
+resource 'vers' (2) {
+	versMajorBCD, versMinor, versKind, versBuild, verUS,
+	versShortString,
+	versShortString
+};
+
