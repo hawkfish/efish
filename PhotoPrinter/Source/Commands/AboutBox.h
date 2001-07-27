c9c5cd9e0fee3c81db87acbf7263f8eeffe6b8cd
hawkfish <hawkfish>
2001-07-27 02:00:21 +0000

    Don't expose dialog.

--- PhotoPrinter/Source/Commands/AboutBox.h
+++ PhotoPrinter/Source/Commands/AboutBox.h
@@ -1,38 +1,24 @@
-#pragma once
-
 /*
 	File:		AboutBox.h
 
-	Contains:	header for the about box
+	Contains:	Interface to the About command handler.
 	
 	Written by:	Dav Lion and David Dunham
 
-	Copyright:	Copyright ©2000 by Electric Fish, Inc.  All Rights reserved.
+	Copyright:	Copyright ©2000-2001 by Electric Fish, Inc.  All Rights Reserved.
 
 	Change History (most recent first):
 
+		27 Jul 2001		rmgw	Don't expose dialog.
 		09 Nov 2000		drd		Dropped gShowing
 		08 Nov 2000		drd		Renamed dialog to AboutBox, subclass EDialog, and added gShowing
 		18 Sep 2000		rmgw	Add registration.
 		21 aug 2000		dml		Created
 */
 
-#include "EDialog.h"
-#include "PhotoAppCommandAttachment.h"
+#pragma once
 
-class AboutBox : public EDialog {
-		LPane*				mRegister;
-		
-	public:
-	
-							AboutBox		(ResIDT			inDialogResID,
-											 LCommander*	inSuper);
-		virtual				~AboutBox		(void);
-	
-		bool				Run				(void);
-};//end class PhotoAboutBox
-	
-	
+#include "PhotoAppCommandAttachment.h"
 
 class AboutCommand : public PhotoAppCommandAttachment
 {
