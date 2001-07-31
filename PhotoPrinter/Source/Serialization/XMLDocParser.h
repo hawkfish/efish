270259a15683ecdb8cfd39f199f94a5e9e1932c2
snapper <snapper>
2001-07-31 08:31:45 +0000

    256 HandleMinMax

--- PhotoPrinter/Source/Serialization/XMLDocParser.h
+++ PhotoPrinter/Source/Serialization/XMLDocParser.h
@@ -8,14 +8,14 @@
 	Copyright:	Copyright ©2000-2001 by Electric Fish, Inc.  All Rights Reserved.
 
 	Change History (most recent first):
-	
+
+		31 Jul 2001		drd		256 HandleMinMax
 		26 Jul 2001		rmgw	Add EUserMessage. Bug #228.
 		26 Jul 2001		rmgw	Factored from PhotoPrintDoc.  Bug #228.
 
 	Description:
 	
 		This is a Builder pattern for PhotoPrintDocs.  
-
 */
 
 #pragma once
@@ -25,15 +25,14 @@
 class PhotoPrintDoc;
 
 class XMLDocParser : public XMLItemParser
-
-	{
-	
+{
 	protected:
 		
 		PhotoPrintDoc*			mDoc;
 		
-		static	void			HandleLayout	(XML::Element &elem, void* userData);	
 		static	void			HandleDocument	(XML::Element &elem, void* userData);	
+		static	void			HandleLayout	(XML::Element &elem, void* userData);	
+		static	void			HandleMinMax	(XML::Element &elem, void* userData);	
 
 		PhotoPrintDoc*			ReadDocument	(XML::Element&	elem);
 
@@ -41,11 +40,10 @@ class XMLDocParser : public XMLItemParser
 		virtual	void			OnDocumentRead	(PhotoPrintDoc*	inDoc);
 
 	public:
-								
 								XMLDocParser	(XML::Input&	inInput,
 												 EUserMessage&	inMessage,
 												 PhotoPrintDoc*	inDoc);
 		virtual					~XMLDocParser	(void);
 
 		virtual	void			ParseDocument	(void);
-	};
+};
