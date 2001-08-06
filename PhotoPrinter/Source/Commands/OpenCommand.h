3ac7e91caf6beef3bee027a28de561140ef649ea
hawkfish <hawkfish>
2001-08-06 01:57:49 +0000

    Put string constants here for easy access.  Bug #286.

--- PhotoPrinter/Source/Commands/OpenCommand.h
+++ PhotoPrinter/Source/Commands/OpenCommand.h
@@ -5,10 +5,11 @@
 
 	Written by:	Dav Lion and David Dunham
 
-	Copyright:	Copyright ©2000-2001 by Electric Fish, Inc.  All Rights reserved.
+	Copyright:	Copyright ©2000-2001 by Electric Fish, Inc.  All Rights Reserved.
 
 	Change History (most recent first):
 
+		06 Aug 2001		rmgw	Put string constants here for easy access.  Bug #286.
 		06 Jul 2001		drd		91 Added OpenDialog to handle filtering etc.
 */
 
@@ -19,6 +20,16 @@
 
 class OpenCommand : public PhotoAppCommandAttachment {
 public:
+
+	enum {
+		si_ImportProblems = 1,
+		si_MixedFiles,
+		
+		si_OpenStringsIndex,
+		
+		strn_OpenStrings = 1102
+		};
+
 						OpenCommand(CommandT inCommand, PhotoPrintApp* inApp);
 	virtual				~OpenCommand(void)		{}
 	
