12a1d77b5133bd9af602530919bf66e5ee00d04b
hawkfish <hawkfish>
2001-10-26 07:48:02 +0000

    First checked in.

---
+++ AEPlugins/aegp/Easy_Cheese/ColorPalVersion.r
@@ -0,0 +1,53 @@
+#include <Types.r>
+
+#define kReleaseBuild		0
+
+#define versMajor			1
+#define versMinor			0
+#define versRevision		0
+#define versBuild			1
+
+#define versMajorBCD		((versMajor/10)*0x10 + (versMajor%10))
+#define versMinorBCD		(versMinor*0x10 + versRevision)
+
+#if kReleaseBuild
+#else
+#endif
+
+#if kReleaseBuild
+	#define versKind			final
+
+	#if versRevision
+		#define versShortString		$$Format("%d.%d.%d", versMajor, versMinor, versRevision)
+	#else
+		#define versShortString		$$Format("%d.%d", versMajor, versMinor)
+	#endif
+#else
+	#define versKind			development
+	#define versKindChar		"d"
+
+	#if versRevision
+		#define versShortString		$$Format("%d.%d.%d" versKindChar "%d", versMajor, versMinor, versRevision, versBuild)
+	#else
+		#define versShortString		$$Format("%d.%d" versKindChar "%d", versMajor, versMinor, versBuild)
+	#endif
+#endif
+
+#define dayBuild			1			
+#define dayBuildString		$$Format("Build %02d%02d%02d.%d", $$Year - 2000, $$Month, $$Day, dayBuild)
+
+resource 'vers' (1, purgeable) {
+	versMajorBCD, versMinorBCD, versKind, versBuild, verUS,
+	versShortString,
+	versShortString " ©2001 Electric Fish, Inc."
+#if !kReleaseBuild
+	"\n" dayBuildString
+#endif
+};
+
+resource 'vers' (2, purgeable) {
+	versMajorBCD, versMinorBCD, versKind, versBuild, verUS,
+	versShortString,
+	"Electric Fish Color Pal"
+};
+
