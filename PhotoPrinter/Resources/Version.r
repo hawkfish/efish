d9485283dd61120d17b6f87eae360b76283568b5
hawkfish <hawkfish>
2001-11-02 09:20:54 +0000

    Handle all debug and release variants.

--- PhotoPrinter/Resources/Version.r
+++ PhotoPrinter/Resources/Version.r
@@ -9,6 +9,7 @@
 
 	Change History (most recent first):
 
+		 2 Nov 2001		rmgw	Handle all debug and release variants.
 		29 Jun 2001		rmgw	Created.
 */
 
@@ -17,26 +18,38 @@
 #define versMajor			1
 #define versMinor			0
 #define versFix				0
-#define versBuild			4
+#define versBuild			0
 
 #define versKind			release
-#define versKindChar		"r"
+//#define versKindChar		"r"
 
 #define versMajorBCD		((versMajor/10)*0x10 + (versMajor%10))
 #define versMinorBCD		(versMinor*0x10 + versFix)
 
-#if versFix
-	#define versShortString		$$Format("%d.%d.%d", versMajor, versMinor, versFix)
+#ifdef versKindChar
+	#if versFix
+		#define versShortString		$$Format("%d.%d.%d" versKindChar "%d", versMajor, versMinor, versFix, versBuild)
+	#else
+		#define versShortString		$$Format("%d.%d" versKindChar "%d", versMajor, versMinor, versBuild)
+	#endif
+	
+	#define dayBuild				1			
+	#define dayBuildString			$$Format("Build %02d%02d%02d.%d", $$Year - 2000, $$Month, $$Day, dayBuild)
 #else
-	#define versShortString		$$Format("%d.%d", versMajor, versMinor)
+	#if versFix
+		#define versShortString		$$Format("%d.%d.%d", versMajor, versMinor, versFix)
+	#else
+		#define versShortString		$$Format("%d.%d", versMajor, versMinor)
+	#endif
 #endif
-
-//#define versShortString		$$Format("%d.%d" versKindChar "%d", versMajor, versMinor, versBuild)
 
 resource 'vers' (1) {
 	versMajorBCD, versMinorBCD, versKind, versBuild, verUS,
 	versShortString,
 	versShortString " ©2001 Electric Fish, Inc."
+#ifdef dayBuildString
+	"\n" dayBuildString
+#endif
 };
 
 resource 'vers' (2) {
