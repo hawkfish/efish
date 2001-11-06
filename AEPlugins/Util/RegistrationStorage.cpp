fd2e6011756c32f1118eae31273cea27fd7b14d8
hawkfish <hawkfish>
2001-11-06 03:24:49 +0000

    first checked in.

---
+++ AEPlugins/Util/RegistrationStorage.cpp
@@ -0,0 +1,223 @@
+/*
+	File:		RegistrationStorage.cp
+
+	Contains:	Implementation of a Serial Number RegistrationFile file.
+
+	Written by:	Richard Wesley
+
+	Copyright:	Copyright ©1999-2001 by Electric Fish, Inc.  All Rights Reserved.
+
+	Change History (most recent first):
+
+         <1>    11/6/01		rmgw    Created from old ERegistrationFile.
+*/
+
+#include "RegistrationStorage.h"
+
+#include "adm_suites.h"
+
+//	===	Class Variables ===
+
+const char
+RegistrationStorage::sTrialVersion [] = "Last Version";
+
+const char
+RegistrationStorage::sTrialDate [] = "Diagonal Size";
+
+const char
+RegistrationStorage::sRegKey [] = "Default Title";
+
+const char
+RegistrationStorage::sHostKey [] = "Host";
+
+// ---------------------------------------------------------------------------
+//		¥ RegistrationStorage
+// ---------------------------------------------------------------------------
+
+RegistrationStorage::RegistrationStorage (
+
+	const	std::string&	inSection,
+	const	ASInt32&		inCurrentVersion)
+	
+	: mPersist (inSection)
+	, mCurrentVersion (inCurrentVersion)
+	
+	{ // begin RegistrationStorage
+		
+	} // end RegistrationStorage
+	
+// ---------------------------------------------------------------------------
+//		¥ IsCorrectHost
+// ---------------------------------------------------------------------------
+
+bool
+RegistrationStorage::IsCorrectHost (void) const
+	
+	{ // begin IsCorrectHost
+	
+		//	Get the info
+		PF_AppPersonalTextInfo	info;
+		PFAppSuite2*			app = (PFAppSuite2*) ADM::Suites::Acquire (kPFAppSuite, kPFAppSuiteVersion2);
+			app->PF_GetPersonalInfo (&info);
+		ADM::Suites::Release (kPFAppSuite, kPFAppSuiteVersion2);
+		
+		//	Get our version
+		char		serial_str[PF_APP_MAX_PERS_LEN + 1];
+		mPersist.GetString (sHostKey, sizeof (serial_str), serial_str, info.serial_str);
+		
+		return (0 == strcmp (serial_str, info.serial_str));
+		
+	} // end IsCorrectHost
+	
+// ---------------------------------------------------------------------------
+//		¥ DeleteHost
+// ---------------------------------------------------------------------------
+
+void
+RegistrationStorage::DeleteHost (void) 
+	
+	{ // begin DeleteHost
+	
+		mPersist.DeleteEntry (sHostKey);
+		
+	} // end DeleteHost
+	
+// ---------------------------------------------------------------------------
+//		¥ GetTrialVersion
+// ---------------------------------------------------------------------------
+
+ASInt32
+RegistrationStorage::GetTrialVersion (void) const
+	
+	{ // begin GetTrialVersion
+	
+		//	Get the version
+		ASInt32				stampVersion;
+		mPersist.GetLong (sTrialVersion, stampVersion, mCurrentVersion);
+		
+		return stampVersion;
+		
+	} // end GetTrialVersion
+	
+// ---------------------------------------------------------------------------
+//		¥ SetTrialVersion
+// ---------------------------------------------------------------------------
+
+void
+RegistrationStorage::SetTrialVersion (
+
+	ASInt32	inTrialVersion)
+	
+	{ // begin SetTrialVersion
+		
+		mPersist.SetLong (sTrialVersion, inTrialVersion);
+			
+	} // end SetTrialVersion
+	
+// ---------------------------------------------------------------------------
+//		¥ DeleteTrialVersion
+// ---------------------------------------------------------------------------
+
+void
+RegistrationStorage::DeleteTrialVersion (void)
+	
+	{ // begin DeleteTrialVersion
+		
+		mPersist.DeleteEntry (sTrialVersion);
+			
+	} // end DeleteTrialVersion
+	
+// ---------------------------------------------------------------------------
+//		¥ GetRegTime
+// ---------------------------------------------------------------------------
+
+time_t
+RegistrationStorage::GetRegTime (void) const
+	
+	{ // begin GetRegTime
+	
+		//	Get the time stamp
+		time_t				stampSecs;
+		mPersist.GetLong (sTrialDate, stampSecs, time (0));
+		
+		return stampSecs;
+		
+	} // end GetRegTime
+	
+// ---------------------------------------------------------------------------
+//		¥ SetRegTime
+// ---------------------------------------------------------------------------
+
+void
+RegistrationStorage::SetRegTime (
+
+	time_t	inRegTime)
+	
+	{ // begin SetRegTime
+		
+		mPersist.SetLong (sTrialDate, inRegTime);
+			
+	} // end SetRegTime
+	
+// ---------------------------------------------------------------------------
+//		¥ DeleteRegTime
+// ---------------------------------------------------------------------------
+
+void
+RegistrationStorage::DeleteRegTime (void)
+	
+	{ // begin DeleteRegTime
+		
+		mPersist.DeleteEntry (sTrialDate);
+			
+	} // end DeleteRegTime
+	
+// ---------------------------------------------------------------------------
+//		¥ GetRegString
+// ---------------------------------------------------------------------------
+
+bool
+RegistrationStorage::GetRegString (
+
+	A_u_long				inBufSize,
+	char*					outValue) const
+	
+	{ // begin GetRegString
+		
+		//	Throw if it is missing
+		if (!mPersist.DoesKeyExist (sRegKey)) return false;
+		
+		//	Return it
+		mPersist.GetString (sRegKey, inBufSize, outValue);
+		
+		return true;
+		
+	} // end GetRegString
+	
+// ---------------------------------------------------------------------------
+//		¥ SetRegString
+// ---------------------------------------------------------------------------
+
+void
+RegistrationStorage::SetRegString (
+
+	const	std::string&	inRegString)
+	
+	{ // begin SetRegString
+		
+		mPersist.SetString (sRegKey, inRegString.c_str ());
+		
+	} // end SetRegString
+	
+// ---------------------------------------------------------------------------
+//		¥ DeleteRegString
+// ---------------------------------------------------------------------------
+
+void
+RegistrationStorage::DeleteRegString (void)
+	
+	{ // begin DeleteRegString
+		
+		mPersist.DeleteEntry (sRegKey);
+			
+	} // end DeleteRegString
---
+++ AEPlugins/Util/RegistrationStorage.h
@@ -0,0 +1,58 @@
+/*
+	File:		RegistrationStorage.h
+
+	Contains:	Interface to a Serial Number Registration storage class.
+
+	Written by:	Richard Wesley
+
+	Copyright:	Copyright ©1999-2001 by Electric Fish, Inc.  All Rights Reserved.
+
+	Change History (most recent first):
+
+         <1>    11/6/01		rmgw    Created from old ERegistrationFile.
+*/
+
+#pragma once
+
+#include "adm_persist.h"
+
+#include <ctime>
+
+using namespace std;
+
+class RegistrationStorage
+
+{
+
+	ADM::Persist		mPersist;
+	const	ASInt32		mCurrentVersion;
+	
+public:
+
+		//	Class Variables
+	static	const char	sHostKey[];
+	static	const char	sTrialVersion[];
+	static	const char	sTrialDate[];
+	static	const char	sRegKey[];
+
+						RegistrationStorage	(const	std::string&	inSection,
+											 const	ASInt32&		inCurrentVersion);
+	
+	bool				IsCorrectHost		(void) const;
+	void				DeleteHost			(void);
+	
+	ASInt32				GetTrialVersion		(void) const;
+	void				SetTrialVersion		(ASInt32				inTrialVersion);
+	void				DeleteTrialVersion	(void);
+	
+	time_t				GetRegTime			(void) const;
+	void				SetRegTime			(time_t					inRegTime);
+	void				DeleteRegTime		(void);
+	
+	bool				GetRegString		(A_u_long				inBufSize,
+									 		 char*					outValue) const;
+	void				SetRegString		(const	std::string&	inRegString);
+	void				DeleteRegString		(void);
+
+};
+
